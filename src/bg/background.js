2d0edbb5f6fb1358e8b063034f9e90dff21bbb8f
Dong Hyun Choi <andy.dh.choi@gmail.com>
2015-06-28 00:48:51 -0500

    add second button on notification

--- src/bg/background.js
+++ src/bg/background.js
@@ -39,19 +39,38 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     }
 });
 
+var notificationID = null;
+
 function showNotification() {
-    chrome.notifications.create({
+    chrome.notifications.create("", {
         type: 'basic',
         iconUrl: chrome.runtime.getURL('icons/icon128.png'),
         title: 'mylk',
         message: 'Before you check out, consider donating $5 to help.',
         priority: 0,
-        buttons: [{ title: 'Go Donate!' }]
+        buttons: [{
+                title: 'Go Donate!'
+            }, {
+                title: 'Check a Related Charity!'
+            }
+        ]
+    }, function(id) {
+        notificationID = id;
     });
 }
 
-chrome.notifications.onButtonClicked.addListener(function () {
-    chrome.tabs.create({
-        url: 'https://api.venmo.com/v1/oauth/authorize?client_id=2713&scope=make_payments'
-    });
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+    //console.log("notifId:" + notifId);
+    //console.log("notifId:" + notifId);
+    if (notifId === notificationID) {
+        if (btnIdx === 0) {
+            chrome.tabs.create({
+                url: 'https://api.venmo.com/v1/oauth/authorize?client_id=2713&scope=make_payments'
+            });
+        } else if (btnIdx === 1) {
+            chrome.tabs.create({
+                url: 'https://www.google.com'
+            });
+        }
+    }
 });
